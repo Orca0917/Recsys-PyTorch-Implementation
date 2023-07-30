4afa0871280f997e93889e42f198ac65345ade24
유종문 <0917jong@gmail.com>
2023-07-30 11:48:56 +0900

    (fix) plotting y-axis range -.2 to .2

--- Factorization Machine (FM)/utils.py
+++ Factorization Machine (FM)/utils.py
@@ -4,17 +4,17 @@ from typing import List
 
 def plot_loss_graph(train_loss: List, test_loss: List):
     fig, train_ax = plt.subplots()
-    train_ax.set_xlabel('Epoch')
-    train_ax.set_ylabel('BCE Loss', color='#1ABDE9')
-    train_ax.plot(train_loss, label='train loss', color='#1ABDE9')
-    train_ax.tick_params(axis='y', labelcolor='#1ABDE9')
-    train_ax.set_ylim(-0.2, 0.85)
+    train_ax.set_xlabel("Epoch")
+    train_ax.set_ylabel("BCE Loss", color="#1ABDE9")
+    train_ax.plot(train_loss, label="train loss", color="#1ABDE9")
+    train_ax.tick_params(axis="y", labelcolor="#1ABDE9")
+    train_ax.set_ylim(0.2, 0.85)
 
     test_ax = train_ax.twinx()
-    test_ax.set_ylabel('AUCROC', color='#F36E8E')
-    test_ax.plot(test_loss, label='test loss', color='#F36E8E')
-    test_ax.tick_params(axis='y', labelcolor='#F36E8E')
-    test_ax.set_ylim(-0.2, 0.85)
+    test_ax.set_ylabel("AUCROC", color="#F36E8E")
+    test_ax.plot(test_loss, label="test loss", color="#F36E8E")
+    test_ax.tick_params(axis="y", labelcolor="#F36E8E")
+    test_ax.set_ylim(0.2, 0.85)
 
     plt.title("FM Train loss & Test AUCROC score")
     plt.show()
