029568780692df0c741180efa08347847b85d05a
유종문 <0917jong@gmail.com>
2023-08-20 02:06:38 +0900

    (feat) model default argument (input_dim, hidden_dim)

--- AutoRec/model.py
+++ AutoRec/model.py
@@ -1,18 +1,21 @@
+import torch
 import torch.nn as nn
 
 
 class AutoRec(nn.Module):
-    def __init__(self, input_dim=10, hidden_dim=4):
+    def __init__(self, input_dim=9724, hidden_dim=500):
         super(AutoRec, self).__init__()
         self.input_dim = input_dim
         self.num_hidden = hidden_dim
 
-        self.encoder = nn.Linear(input_dim, hidden_dim)
+        self.encoder = nn.Linear(input_dim, hidden_dim, dtype=torch.float64)
+        self.sigmoid = nn.Sigmoid()
         self.dropout = nn.Dropout(p=0.5)
-        self.decoder = nn.Linear(hidden_dim, input_dim)
+        self.decoder = nn.Linear(hidden_dim, input_dim, dtype=torch.float64)
 
     def forward(self, x):
         x = self.encoder(x)
+        x = self.sigmoid(x)
         x = self.dropout(x)
         x = self.decoder(x)
 
