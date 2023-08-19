b06285673500bc27ff456c1805623602e9185b37
유종문 <0917jong@gmail.com>
2023-08-20 02:06:19 +0900

    (test) hyperparameter update

--- AutoRec/hparams.py
+++ AutoRec/hparams.py
@@ -1,10 +1,11 @@
-data_path = ''
-save_path = ''
-data_url = ''
-data_base_path = ''
+# Data
+save_path = "./data/"
+data_path = "./data/ml-latest-small.zip"
+data_base_path = "./data/ml-latest-small/"
+data_url = "https://files.grouplens.org/datasets/movielens/ml-latest-small.zip"
 
 batch_size = 16
-input_dim = 1000
-hidden_dim = 4
-learning_rate = .001
+input_dim = 9724
+hidden_dim = 500
+learning_rate = 0.001
 epoch = 100
