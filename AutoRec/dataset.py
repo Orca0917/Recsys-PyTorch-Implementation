ff894d9713161ade34c6078e9c6119e6b1da4f04
유종문 <0917jong@gmail.com>
2023-08-20 02:05:44 +0900

    (feat) autorec dataset (using numpy matrix)

--- AutoRec/dataset.py
+++ AutoRec/dataset.py
@@ -2,6 +2,7 @@ import os
 import zipfile
 import requests
 import hparams
+import numpy as np
 import pandas as pd
 import torch.utils.data as data
 
@@ -9,13 +10,13 @@ import torch.utils.data as data
 class AutoRecDataset(data.Dataset):
     def __init__(self, is_train=True):
         self.is_train = is_train
-        self.data_df = self.get_dataset(self.is_train)
+        self.data_df: np.ndarray = self.get_dataset(self.is_train)
 
     def __len__(self):
         return len(self.data_df)
 
     def __getitem__(self, index):
-        return self.data_df[index]
+        return self.data_df[index, :]
 
     def download_dataset(self):
         if os.path.isdir(hparams.save_path):
@@ -27,12 +28,12 @@ class AutoRecDataset(data.Dataset):
 
         get_response = requests.get(hparams.data_url)
         file_name = hparams.data_url.split("/")[-1]
-        with open(os.path.join('./data', file_name), 'wb') as f:
+        with open(os.path.join("./data", file_name), "wb") as f:
             for chunk in get_response.iter_content(chunk_size=None):
                 f.write(chunk)
         print("Movielens dataset (ml-latest-small.zip) download completed.")
 
-        with zipfile.ZipFile(hparams.data_path, mode='r') as z:
+        with zipfile.ZipFile(hparams.data_path, mode="r") as z:
             z.extractall(hparams.save_path)
         print("ml-latest-small.zip extraction(unzip) done.")
 
@@ -40,16 +41,38 @@ class AutoRecDataset(data.Dataset):
         print("Removing zip file completed.")
 
     def get_dataset(self, is_train):
-        df: pd.DataFrame = pd.read_csv(os.path.join(hparams.data_base_path, 'ratings.csv')).to_numpy()[:, :3]
-        user_list = df['userId'].unique().tolist()
-        n_user = len(user_list)
 
-        train_user = user_list[:n_user * 0.8]
-        test_user = user_list[n_user * 0.8:]
+        self.download_dataset()
+
+        df: pd.DataFrame = pd.read_csv(
+            os.path.join(hparams.data_base_path, "ratings.csv"),
+        )
+        df.drop(columns=["timestamp"], inplace=True)
+
+        user2idx, item2idx = {}, {}
+        for idx, user in enumerate(df["userId"].unique().tolist()):
+            user2idx[user] = idx
+
+        for idx, item in enumerate(df["movieId"].unique().tolist()):
+            item2idx[item] = idx
+
+        df["userId"] = df["userId"].map(user2idx)
+        df["movieId"] = df["movieId"].map(item2idx)
+
+        n_transaction = len(df["userId"])
+        train_idx = int(n_transaction * 0.8)
+        n_user = df["userId"].nunique()
+        n_item = df["movieId"].nunique()
 
         if is_train:
-            df = df[df['userId'] in train_user]
+            df = df.iloc[:train_idx, :]
         else:
-            df = df[df['userId'] in test_user]
+            df = df.iloc[train_idx:, :]
 
-        return df
+        data_mat = np.zeros((n_user, n_item))
+        for _, (user, item, rating) in df.iterrows():
+            user = int(user)
+            item = int(item)
+            data_mat[user, item] = rating
+
+        return data_mat
